1b47f2f0afdbca1a3a81b82d6f5d2eafd8bd4ee6
miyamoto KAZuyuki <k-miyamoto@pu.jp.nec.com>
2020-01-31 22:31:33 +0900

    Update hvsv.c to run solely

--- src/hbsv.c
+++ src/hbsv.c
@@ -1,3 +1,9 @@
+/*
+ * Usage:
+ *
+ *	hbsv PORT_SELF PORT_TARGET
+ *
+ */
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -7,14 +13,14 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <netdb.h>
 
-#define PORT    5000
-
-int main()
+int main(int argc, char *argv[])
 {
 	int sock = 0;
 	ssize_t bytes_read;
-	struct sockaddr_in server_addr, client_addr;
+	struct hostent *host;
+	struct sockaddr_in server_addr, client_addr, reply_addr;
 	socklen_t addr_len = sizeof(struct sockaddr_in);
 	fd_set fds, readfds;
 	struct timeval tv;
@@ -25,11 +31,16 @@ int main()
 		exit(1);
 	}
 
+	host = (struct hostent *) gethostbyname((char *) "127.0.0.1");
+	client_addr.sin_family = AF_INET;
+	client_addr.sin_port = htons(atoi(argv[2]));
+	client_addr.sin_addr = *((struct in_addr*) host->h_addr);
+	bzero(&(client_addr.sin_zero), 8);
+
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(PORT);
+	server_addr.sin_port = htons(atoi(argv[1]));
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 	bzero(&(server_addr.sin_zero), 8);
-
 	if (bind(sock, (struct sockaddr *) &server_addr, sizeof(struct sockaddr)) == -1) {
 		perror(" bind");
 		exit(1);
@@ -38,25 +49,39 @@ int main()
 	FD_ZERO(&readfds);
 	FD_SET(sock, &readfds);
 
-	printf("\n Waiting for client on port 5000\n");
+	printf("\nReceiving on port %s\n", argv[1]);
 	while (1) {
+		// fds ad tv need to re-set cz these are destroyed on select()
 		tv.tv_sec = 0;
-		tv.tv_usec = 100 * 1000; // 100ms
+		tv.tv_usec = 3000 * 1000;	// 3000ms
 		memcpy(&fds, &readfds, sizeof(fd_set));
-		if ( 0 > select(sock + 1, &fds, NULL, NULL, &tv)) {
+		if (0 > select(sock + 1, &fds, NULL, NULL, &tv)) {
 			perror("select");
 			exit(1);
 		}
 		if (FD_ISSET(sock, &fds)) {
-			bytes_read = recvfrom(sock, &idx, sizeof(unsigned short), 0, (struct sockaddr *) &client_addr, &addr_len);
+			addr_len = sizeof(struct sockaddr_in);
+			bytes_read = recvfrom(sock, &idx, sizeof(unsigned short), 0, (struct sockaddr *) &reply_addr, &addr_len);
 			if (bytes_read == -1) {
 				perror("recvfrom");
 				exit(1);
 			}
-			printf("receive %d bytes data from client: %d\n", bytes_read, idx);
-		} else {
+			//printf("receive %d bytes data from %s:%d idx = %d\n",
+			//	bytes_read, inet_ntoa(reply_addr.sin_addr), ntohs(reply_addr.sin_port), idx);
+
+			idx++;
+
+		}
+		else {
 			printf("#### TIMEOUT ####\n");
 		}
+
+		addr_len = sizeof(struct sockaddr_in);
+		sendto(sock, &idx, sizeof(unsigned short), 0, (struct sockaddr*) &client_addr, addr_len);
+		printf("sent    %d bytes data to   %s:%d idx = %d\n",
+			sizeof(short), inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), idx);
+
+		usleep(2000*1000);	// 2000ms
 	}
 	close(sock);
 	return 0;
