195276a4f1b2a4b3ed4080d55d950d57fdd73579
Miyamoto Kazuyuki <m.kazuyuki@gmail.com>
2025-04-24 03:37:54 +0900

    Update hbsv.c

--- src/hbsv.c
+++ src/hbsv.c
@@ -16,7 +16,7 @@ int main()
 	ssize_t bytes_read;
 	struct sockaddr_in server_addr, client_addr;
 	socklen_t addr_len = sizeof(struct sockaddr_in);
-	fd_set fds, readfds;
+	fd_set readfds;
 	struct timeval tv;
 	unsigned short int idx = 0;
 
@@ -35,25 +35,23 @@ int main()
 		exit(1);
 	}
 
-	FD_ZERO(&readfds);
-	FD_SET(sock, &readfds);
-
 	printf("\n Waiting for client on port 5000\n");
 	while (1) {
 		tv.tv_sec = 0;
 		tv.tv_usec = 100 * 1000; // 100ms
-		memcpy(&fds, &readfds, sizeof(fd_set));
-		if ( 0 > select(sock + 1, &fds, NULL, NULL, &tv)) {
+		FD_ZERO(&readfds);
+		FD_SET(sock, &readfds);
+		if ( 0 > select(sock + 1, &readfds, NULL, NULL, &tv)) {
 			perror("select");
 			exit(1);
 		}
-		if (FD_ISSET(sock, &fds)) {
+		if (FD_ISSET(sock, &readfds)) {
 			bytes_read = recvfrom(sock, &idx, sizeof(unsigned short), 0, (struct sockaddr *) &client_addr, &addr_len);
 			if (bytes_read == -1) {
 				perror("recvfrom");
 				exit(1);
 			}
-			printf("receive %d bytes data from client: %d\n", bytes_read, idx);
+			printf("receive %ld bytes data from client: %d\n", bytes_read, idx);
 		} else {
 			printf("#### TIMEOUT ####\n");
 		}
